c5b9a36256bbeb564ca3cbdfb9856015e6dbf4e4
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-02 04:30:46 +0700

    [refactor] clean code server

--- server.js
+++ server.js
@@ -1,96 +1,21 @@
 const express = require("express");
-const db = require("./database") 
+const path = require("path");
 const bodyParser = require("body-parser");
-
 const app = express();
 
+// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.set("view engine", "ejs");
-app.set("views", "views");
+app.set("views", path.join(__dirname, "views"));
 
-// const mysql = require("mysql2")
-// const db = mysql.createConnection({
-//   host: "localhost",
-//   database: "pandhu",
-//   user: "root",
-//   password: "",
-// });
+// Mengimpor file rute
+const userRoutes = require("./routes/user.route");
 
-app.get("/", (req, res) => {
-  const sql = `SELECT * FROM users`;
-  db.query(sql, (err, result) => {
-    const users = JSON.parse(JSON.stringify(result));
-    res.render("index", { users: users, title: "Daftar Siswa" });
-  });
-});
+// Menggunakan rute-rute dalam aplikasi
+app.use("/", userRoutes);
 
-app.get("/tambah", (req, res) => {
-  res.render("tambah");
-});
-
-app.post("/tambah", (req, res) => {
-  const insertSql = `INSERT INTO users (nim, nama_lengkap, kelas, alamat, email) VALUES 
-    ('${req.body.nim}', '${req.body.nama_lengkap}', '${req.body.kelas}', '${req.body.alamat}', '${req.body.email}');`;
-
-  db.query(insertSql, (err, result) => {
-    if (err) throw err;
-    res.redirect("/");
-  });
-});
-
-app.get("/update/:id", (req, res) => {
-  const sql = `SELECT * FROM users WHERE id = ${req.params.id}`;
-  db.query(sql, (err, result) => {
-    const userData = JSON.parse(JSON.stringify(result));
-    res.render("update", { users: userData });
-  });
-});
-
-app.post("/update/:id", (req, res) => {
-  const id = req.params.id;
-  const updateSql = `UPDATE users SET nim='${req.body.nim}', nama_lengkap='${req.body.nama_lengkap}', kelas='${req.body.kelas}', alamat='${req.body.alamat}', email='${req.body.email}' WHERE id = ${id}`;
-
-  db.query(updateSql, (err, result) => {
-    if (err) {
-      console.error(err);
-    } else {
-      res.redirect("/");
-    }
-  });
-});
-
-app.get("/delete/:id", (req, res) => {
-  const id_siswa = req.params.id; 
-  if (id_siswa) {
-    if (isNaN(id_siswa)) {
-      res.status(400).json({ message: "ID siswa tidak valid" });
-      return;
-    }
-
-    const deleteSql = `DELETE FROM users WHERE id = ${id_siswa}`;
-
-    db.query(deleteSql, (err, result) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ message: "Gagal menghapus siswa" });
-      } else {
-        res.redirect('/')
-      }
-    });
-  } else {
-    res.status(400).json({ message: "ID siswa tidak valid" });
-  }
-});
-
-db.connect((err) => {
-  if (err) {
-    console.log("Cant connect to database" + "\n" + err.message);
-    return;
-  }
-  console.log("Database is connected...");
-  app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-  });
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
